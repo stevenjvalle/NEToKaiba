2ee1d1d96c6f2b8a67f57ebde99e0eb268d9aafd
Andrew Nipp <andrewnipp@yahoo.com>
2022-08-22 14:15:35 -0700

    Got lavalink up and running, added audio module

---
+++ NamazuKingdom/Modules/AudioModule.cs
@@ -0,0 +1,76 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Victoria;
+
+namespace NamazuKingdom.Modules
+{
+    public sealed class AudioModule : ModuleBase<SocketCommandContext>
+    {
+        private readonly LavaNode _lavaNode;
+        public AudioModule(LavaNode node)
+        {
+            _lavaNode = node;
+        }
+        [Command("join")]
+        public async Task JoinAsync()
+        {
+            if (_lavaNode.HasPlayer(Context.Guild))
+            {
+                await ReplyAsync("I'm already connected to a voice channel!");
+                return;
+            }
+
+            var voiceState = Context.User as IVoiceState;
+            if (voiceState?.VoiceChannel == null)
+            {
+                await ReplyAsync("You must be connected to a voice channel!");
+                return;
+            }
+
+            try
+            {
+                await _lavaNode.JoinAsync(voiceState.VoiceChannel, Context.Channel as ITextChannel);
+                await ReplyAsync($"Joined {voiceState.VoiceChannel.Name}!");
+            }
+            catch (Exception exception)
+            {
+                await ReplyAsync(exception.Message);
+            }
+        }
+
+        [Command("leave")]
+        public async Task LeaveAsync()
+        {
+            if (!_lavaNode.HasPlayer(Context.Guild))
+            {
+                await ReplyAsync("I'm not connected to a voice channel!");
+                return;
+            }
+
+            var voiceState = Context.User as IVoiceState;
+            if (voiceState?.VoiceChannel == null)
+            {
+                await ReplyAsync("You must be connected to a voice channel!");
+                return;
+            }
+
+            try
+            {
+                await _lavaNode.LeaveAsync(voiceState.VoiceChannel);
+                await ReplyAsync($"Left {voiceState.VoiceChannel.Name}!");
+            }
+            catch (Exception exception)
+            {
+                await ReplyAsync(exception.Message);
+            }
+        }
+
+    }
+}
---
+++ NamazuKingdom/Program.cs
@@ -0,0 +1,86 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Victoria;
+
+namespace NamazuKingdom // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        private readonly IServiceProvider _serviceProvider;
+        private readonly IConfiguration _configuration;
+
+        public Program()
+        {
+            _configuration = new ConfigurationBuilder()
+                .AddEnvironmentVariables(prefix: "DC_")
+                .AddJsonFile("appsettings.json", optional: true)
+                .Build();
+            _serviceProvider = CreateProvider(_configuration);
+        }
+
+        static void Main(string[] args)
+            => new Program().RunAsync(args).GetAwaiter().GetResult();
+
+        static IServiceProvider CreateProvider(IConfiguration config)
+        {
+            var socketConfig = new DiscordSocketConfig();
+            var collection = new ServiceCollection()
+                .AddSingleton(socketConfig)
+                .AddSingleton<DiscordSocketClient>()
+                .AddSingleton<CommandService>()
+                .AddSingleton(config)
+                .AddSingleton<LavaConfig>()
+                .AddSingleton<LavaNode>();
+            return collection.BuildServiceProvider();
+        }
+
+        async Task RunAsync(string[] args)
+        {
+            //StartLavalink();
+            // One of the more flexable ways to access the configuration data is to use the Microsoft's Configuration model,
+            // this way we can avoid hard coding the environment secrets. I opted to use the Json and environment variable providers here.
+
+            // Request the instance from the client.
+            // Because we're requesting it here first, its targetted constructor will be called and we will receive an active instance.
+            var client = _serviceProvider.GetRequiredService<DiscordSocketClient>();
+
+            CommandHandler handler = new(_serviceProvider);
+            await handler.InstallCommandsAsync();
+
+            client.Log += async (msg) =>
+            {
+                await Task.CompletedTask;
+                Console.WriteLine(msg);
+            };
+            client.Ready += OnReadyAsync;
+            await client.LoginAsync(TokenType.Bot, _configuration["token"]);
+            await client.StartAsync();
+
+            //Start lavalink
+
+            await Task.Delay(Timeout.Infinite);
+        }
+
+        private void StartLavalink()
+        {
+            System.Diagnostics.Process proc = new System.Diagnostics.Process();
+            proc.StartInfo.FileName = _configuration["lavalinkFolder"] + "\\run.bat";
+            proc.StartInfo.WorkingDirectory = _configuration["lavalinkFolder"];
+            proc.Start();
+        }
+
+        private async Task OnReadyAsync()
+        {
+            var lavaNode = _serviceProvider.GetRequiredService<LavaNode>();
+            if (!lavaNode.IsConnected)
+            {
+                await lavaNode.ConnectAsync();
+                Console.WriteLine(lavaNode.IsConnected);
+            }
+        }
+    }
+}

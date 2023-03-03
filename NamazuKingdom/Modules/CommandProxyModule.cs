b9c3704f764350bd839edd9699569f08f883d562
Steven Valle <stevenjvalle@gmail.com>
2023-03-03 18:39:34 -0500

    Revert "Remove unnecessary console lines."
    This reverts commit f1de8b0f54255f66585b0ef8e4697ee7522d7bfb.

--- NamazuKingdom/Modules/CommandProxyModule.cs
+++ NamazuKingdom/Modules/CommandProxyModule.cs
@@ -22,10 +22,11 @@ namespace NamazuKingdom.Modules
             _databaseServices = dbServices;
         }
 
-        // TEMPORARY: Need to come up with a static model so we don't have two versions of the same functionality floating about.
         [Command("tts")]
         public async Task TTSAsync([Remainder] string sound)
         {
+            Console.WriteLine("TTS Started:");
+            Console.WriteLine(sound);
             //todo null handle
             if (Context.User == null) return;
 
@@ -36,22 +37,29 @@ namespace NamazuKingdom.Modules
                 await ReplyAsync("Please provide search terms.");
                 return;
             }
+            Console.WriteLine("Pass null or white space test. ");
+            Console.WriteLine("Guild ID:");
+            Console.WriteLine(Context.Guild.Id);
             if (_audioService.IsConnected(Context.Guild.Id) == false)
             {
                 await ReplyAsync("Audio client is null, am I connected to a voice channel?");
                 return;
             }
+            Console.WriteLine("Pass Guild Connection test. ");
             if (string.IsNullOrWhiteSpace(sound))
                 return;
             if (sound.Contains("https://") || sound.Contains("http://"))
                 return;
+            Console.WriteLine("Pass URL. ");
             if ((Context.User as IGuildUser)?.VoiceChannel == null ||
                 (Context.User as IGuildUser)?.VoiceChannel != _audioService.GetCurrentVoiceChannel(Context.Guild.Id))
             {
                 await ReplyAsync("You are not connected to the same voice channel as the bot.");
                 return;
             }
+            Console.WriteLine("Test this");
             var url = $"https://api.streamelements.com/kappa/v2/speech?voice={botVoice}&text={StringHelpers.CleanTTSString(sound)}";//+StringHelpers.CleanTTSString(sound);
+            Console.WriteLine(url);
             await _audioService.SendAsync(url, Context.Guild.Id);
         }
     }
